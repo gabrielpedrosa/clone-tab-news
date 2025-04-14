6081a21afc78e0109b58084ee93855dc9df6e785
gabrielpedrosa <gabriel.pedrosaf@gmail.com>
2025-04-14 09:17:49 -0300

    feat: finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,33 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum"); 
+  const updateAt = new Date().toISOString();
 
-  console.log(result); 
-  response.status(200).json({ chave: "Show!" });
+  const versionResult = await database.query("SHOW server_version;")
+  const version = versionResult.rows[0].server_version
+ 
+  const maxConnectionsResult = await database.query("SHOW max_connections;")
+  const maxConnections = maxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB || "postgres";
+  const openedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  })
+  const openedConnections = openedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      database: { 
+        version: version,
+        max_connections: parseInt(maxConnections),
+        opened_connections: openedConnections,
+      },
+    },
+  });
+
+  
 }
 
 export default status;
