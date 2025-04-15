bc1afec9c3b41f6a05f96a7303d679f98ca7fa03
gabrielpedrosa <gabriel.pedrosaf@gmail.com>
2025-04-15 18:24:58 -0300

    feat: handle database SSL connection

--- infra/database.js
+++ infra/database.js
@@ -5,7 +5,8 @@ async function query(queryObj) {
         port: process.env.POSTGRES_PORT,
         user: process.env.POSTGRES_USER,
         database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD
+        password: process.env.POSTGRES_PASSWORD,
+        ssl: process.env.NODE_ENV === 'development' ? false : true
     });
     
     try {
