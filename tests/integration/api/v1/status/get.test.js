21f0c197a68898b599d3b4339272a70b677ded48
gabrielpedrosa <gabriel.pedrosaf@gmail.com>
2025-05-08 11:17:41 -0300

    feat: change `postgresSQL` version

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -14,7 +14,7 @@ test("GET to /api/v1/status should return 200", async () => {
   const expectedDate = new Date(body.updated_at).toISOString()
   expect(body.updated_at).toBe(expectedDate);
 
-  expect(body.dependencies.database.version).toEqual("16.0")
+  expect(body.dependencies.database.version).toEqual("17.0")
   expect(body.dependencies.database.max_connections).toEqual(100)
   expect(body.dependencies.database.opened_connections).toEqual(1)
   expect(body.dependencies.database).toEqual({
